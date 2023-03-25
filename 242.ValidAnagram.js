e024d60ac58aa475c1c9304f3e361ed0ecb171b0
Nyah Macklin <60084237+MacklinEngineering@users.noreply.github.com>
2023-03-25 17:27:09 -0400

    Update 242.ValidAnagram.js

--- 242.ValidAnagram.js
+++ 242.ValidAnagram.js
@@ -3,6 +3,8 @@
  * @param {string} t
  * @return {boolean}
  
+ https://leetcode.com/problems/valid-anagram/
+ 
  Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
