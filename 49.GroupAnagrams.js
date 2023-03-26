98997113f38f3bf4d71fbeb317de656bdb5f3e2f
Nyah Macklin <60084237+MacklinEngineering@users.noreply.github.com>
2023-03-26 14:42:52 -0400

    Create 49.GroupAnagrams.js

---
+++ 49.GroupAnagrams.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ https://leetcode.com/problems/group-anagrams/description/
+ 
+ Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+ */
+var groupAnagrams = function(strs) {
+    let map = {}
+    for (let str of strs){
+        let sortedKey = str.split("").sort().join("") //aet
+        !map[sortedKey] ? map[sortedKey] = [str] : map[sortedKey].push(str) 
+    }
+    return Object.values(map)
+};
