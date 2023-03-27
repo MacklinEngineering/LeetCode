b0bdb3df1a31b6129936ca416683bcd20c494c17
Nyah Macklin <60084237+MacklinEngineering@users.noreply.github.com>
2023-03-27 10:14:49 -0400

    Create 125.ValidPalindrome.js

---
+++ 125.ValidPalindrome.js
@@ -0,0 +1,42 @@
+https://leetcode.com/problems/valid-palindrome/description/
+
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ 
+ A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+ */
+var isPalindrome = function(s) {
+   const str = s.replaceAll(/[^A-Za-z0-9]/g,'').toLowerCase()
+   
+   const rev = str.split("").reverse().join("")
+
+   if (str === rev){
+       return true
+   }else{
+       return false
+   }
+};
